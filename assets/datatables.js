0750256f5fe8eecdc0bc4a7d936a546f1349bdc0
Daniel Adámek <adamedan@fit.cvut.cz>
2022-02-23 23:07:29 +0100

    datatables update and api update with entities

---
+++ assets/datatables.js
@@ -0,0 +1,12 @@
+require( 'jquery' );
+require( 'jszip' );
+require( 'pdfmake' );
+require( 'datatables.net-bs' )();
+require( 'datatables.net-buttons-bs' )();
+require( 'datatables.net-buttons/js/buttons.html5.js' )();
+require( 'datatables.net-buttons/js/buttons.print.js' )();
+require( 'datatables.net-colreorder-bs' )();
+require( 'datatables.net-datetime' )();
+require( 'datatables.net-fixedcolumns-bs' )();
+require( 'datatables.net-fixedheader-bs' )();
+require( 'datatables.net-responsive-bs' )();
