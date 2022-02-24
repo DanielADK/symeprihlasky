487bcd0be7a43b876ee8417525719020352c1e70
Daniel Adámek <adamedan@fit.cvut.cz>
2022-02-24 23:47:36 +0100

    person list added

--- src/Controller/Admin/PersonController.php
+++ src/Controller/Admin/PersonController.php
@@ -8,21 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PersonController extends AbstractController {
 
-    #[Route('/admin/uzivatele/seznam', name: 'admin_person_list')]
+    #[Route('/admin/uzivatele/seznam', name: 'admin_person_view')]
     public function index(): Response {
-        return $this->render('Admin/Person/list.html.twig', [
+        return $this->render('Admin/Person/view.html.twig', [
             "section" => "person",
             "page_name" => "Seznam uživatelů",
             "page_path" => array("Domov", "Uživatelé", "Seznam")
         ]);
     }
 
-    #[Route('/admin/uzivatele/nahled/{id}', name: 'admin_person_view')]
+    #[Route('/admin/uzivatele/uprava/{id}', name: 'admin_person_edit')]
     public function view(int $id): Response {
         return $this->render('Admin/home.html.twig', [
             "section" => "person",
-            "page_name" => "Uživatel ID: ".$id,
-            "page_path" => array("Domov", "Uživatelé", "Náhled uživatele")
+            "page_name" => "Úprava uživatele ID: ".$id,
+            "page_path" => array("Domov", "Uživatelé", "Úprava uživatele")
         ]);
     }
 }
