0f8074095da6853e2a750aea74f8a906d0aa92ef
Daniel Adámek <adamedan@fit.cvut.cz>
2022-05-01 13:49:50 +0200

    Event preparation + person bugfixes- list is done, view is prepared

--- src/Controller/Admin/EventController.php
+++ src/Controller/Admin/EventController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use Composer\DependencyResolver\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +20,18 @@ class EventController extends AbstractController {
         ]);
     }
 
-    #[Route('/admin/akce/nahled/{id}', name: 'admin_event_view')]
-    public function view(int $id): Response {
-        return $this->render('Admin/home.html.twig', [
+    #[Route('/admin/akce/zobrazit/{id}', name: 'admin_event_view')]
+    public function view(int $id, EntityManagerInterface $em, Request $request): Response {
+        $event = $em->getRepository("App:Event")->findOneBy(
+            array("id" => $id)
+        );
+        if ($event == null) {
+            $this->addFlash("warning", "Tato akce nebyla nalezena!");
+            return new RedirectResponse($this->generateUrl("admin_event_list"));
+        }
+        return $this->render('Admin/Event/view.html.twig', [
             "section" => "event",
+            "event" => $event,
             "page_name" => "Náhled akce ID: ".$id,
             "page_path" => array("Domov", "Akce", "Náhled akce")
         ]);
