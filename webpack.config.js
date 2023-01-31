02f4a34b9918afadf27d5f754ccf008440b42f75
Daniel Adámek <daniel.adamek123@icloud.com>
2023-01-31 14:08:30 +0100

    css -> scss

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,7 @@ Encore
     .enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableTypeScriptLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
